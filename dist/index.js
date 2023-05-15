fd81ca6732af3059c545a164c9f7204cf766ea06
Thanos Korakas <thanoskorakas@gmail.com>
2023-05-15 23:02:43 +0300

    test: check gh context

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world!!');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+async function run() {
+    const currentRepo = process.env.GITHUB_REPOSITORY;
+    // const githubToken = core.getInput('github-token');
+  
+    console.log('hello world', currentRepp);
+    console.log('context', github.context);
+};
+
+
+run();
